c66f93f22906cda23dba674c6018486330070896
pandasumm <pandasumm@gmail.com>
2018-04-10 18:42:37 -0500

    sample for firebase using

--- src/pages/stop-details/stop-details.ts
+++ src/pages/stop-details/stop-details.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 
+import { AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
 /**
- * Generated class for the StopDetailsPage page.
+ * Generated class for the StopDetailsPage  page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -15,12 +18,23 @@ import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 })
 export class StopDetailsPage {
   public stopName: string;
-
-  constructor(public navParams: NavParams, public view: ViewController) {
+  public stopLine: string;
+  public stopID: string;
+  stopTime: Observable<any[]>;
+  constructor(public navParams: NavParams, public view: ViewController, public firebaseProvider: FirebaseProvider) {
   }
 
   ionViewDidLoad() {
     this.stopName = this.navParams.data.stopName;
+    this.stopLine = this.navParams.data.stopLine;
+    var d = new Date();
+    this.stopID = this.navParams.data.stopID;
+    this.stopTime = this.firebaseProvider.getstopTimes(this.stopID, d.getDay()).valueChanges();
+
+    // basic way to parse data from stopTime:
+    this.stopTime.subscribe(res => { res.forEach(t => { console.log("each time and line: " + t.time + " " + t.line) }); });
+
+    console.log("stopDetail id: " + this.stopID);
   }
 
   closeModal() {
